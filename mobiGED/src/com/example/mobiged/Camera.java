80db70b1766e25b2b2d382429f2404810638de1a
Matthieu <Matthieu@172.16.82.93>
2014-01-08 09:45:07 +0100

    test

--- mobiGED/src/com/example/mobiged/Camera.java
+++ mobiGED/src/com/example/mobiged/Camera.java
@@ -63,7 +63,7 @@ public class Camera extends Activity {
 
 		imgPreview = (ImageView) findViewById(R.id.imgPreview);
 
-		// Button
+		// Buttonffff
 		exit = (Button) findViewById(R.id.bt_photo_exit);
 		crop = (Button) findViewById(R.id.bt_photo_crop);
 		valid = (Button) findViewById(R.id.bt_photo_valid);
